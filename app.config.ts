896b7d02313537dd328d10e160d5462dc79fbed7
Leonard Takada <leonardtakada@yahoo.com>
2026-09-26 02:30:29 -0700

    build: bump ios buildNumber to 2

--- app.config.ts
+++ app.config.ts
@@ -44,7 +44,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "1",
+    buildNumber: "2",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
